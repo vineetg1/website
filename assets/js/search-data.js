c7140e3dd69229652ed8fc835cbfd7d88d429246
vineet-gandhi <gandhivineet@gmail.com>
2025-05-26 12:53:40 +0000

    Deploying to gh-pages from @ vineet-gandhi/vineet-gandhi.github.io@de79e7076c2234f43a715dee13120acf9dd7b6e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
+        },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
           description: "",
           section: "Navigation",
           handler: () => {
